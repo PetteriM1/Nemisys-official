ab1af85ee23c5a7cf55d0aa2dd606a6bbefd415a
Woder <17339354+wode490390@users.noreply.github.com>
2021-10-10 01:18:41 +0800

    Update to 1.17.30 (#77)

--- src/main/java/org/itxtech/nemisys/utils/Skin.java
+++ src/main/java/org/itxtech/nemisys/utils/Skin.java
@@ -39,10 +39,12 @@ public class Skin {
     private boolean premium;
     private boolean persona;
     private boolean capeOnClassic;
+    private boolean primaryUser = true;
     private String capeId;
     private String skinColor = "#0";
     private String armSize = "wide";
     private boolean trusted;
+    private String geometryDataEngineVersion = "";
 
     public boolean isValid() {
         return true; //TODO: some sort of validation
@@ -201,6 +203,22 @@ public class Skin {
 
     public void setCapeOnClassic(boolean capeOnClassic) {
         this.capeOnClassic = capeOnClassic;
+    }
+
+    public void setPrimaryUser(boolean primaryUser) {
+        this.primaryUser = primaryUser;
+    }
+
+    public boolean isPrimaryUser() {
+        return primaryUser;
+    }
+
+    public void setGeometryDataEngineVersion(String geometryDataEngineVersion) {
+        this.geometryDataEngineVersion = geometryDataEngineVersion;
+    }
+
+    public String getGeometryDataEngineVersion() {
+        return geometryDataEngineVersion;
     }
 
     public boolean isTrusted() {
